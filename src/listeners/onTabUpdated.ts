3debb6ee4ddc4ab1a3d196edcd59e76bb7a354c6
2zcory <khanhtri009@gmail.com>
2026-04-18 18:18:56 +0700

    fix(listeners): ensure real-time updates for tab removal and group changes

--- src/listeners/onTabUpdated.ts
+++ src/listeners/onTabUpdated.ts
@@ -9,13 +9,25 @@ const onTabUpdated = (callback: Callback) => {
     chrome.tabs.onActivated.addListener(callback)
     chrome.tabs.onUpdated.addListener(callback)
     chrome.tabs.onMoved.addListener(callback)
+    chrome.tabs.onRemoved.addListener(callback)
+    chrome.tabs.onCreated.addListener(callback)
+    chrome.tabs.onAttached.addListener(callback)
+    chrome.tabs.onDetached.addListener(callback)
     chrome.tabGroups.onUpdated.addListener(callback)
+    chrome.tabGroups.onCreated.addListener(callback)
+    chrome.tabGroups.onRemoved.addListener(callback)
 
     return () => {
       chrome.tabs.onActivated.removeListener(callback)
       chrome.tabs.onUpdated.removeListener(callback)
       chrome.tabs.onMoved.removeListener(callback)
+      chrome.tabs.onRemoved.removeListener(callback)
+      chrome.tabs.onCreated.removeListener(callback)
+      chrome.tabs.onAttached.removeListener(callback)
+      chrome.tabs.onDetached.removeListener(callback)
       chrome.tabGroups.onUpdated.removeListener(callback)
+      chrome.tabGroups.onCreated.removeListener(callback)
+      chrome.tabGroups.onRemoved.removeListener(callback)
     }
   }, [])
 }
