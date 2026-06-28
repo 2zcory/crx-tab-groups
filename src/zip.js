d803a2fd07170525beffc160c7549176838424a6
Coryyyyy <khanhtri009@gmail.com>
2026-06-28 11:11:38 +0700

    chore: read package version and set hardcoded prefix in zip.js

--- src/zip.js
+++ src/zip.js
@@ -2,9 +2,9 @@ import gulp from 'gulp'
 import zip from 'gulp-zip'
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
-const manifest = require('../build/manifest.json')
+const pkg = require('../package.json')
 
 gulp
   .src('build/**', { encoding: false })
-  .pipe(zip(`${manifest.name.replaceAll(':', '').replaceAll(' ', '-')}-${manifest.version}.zip`))
+  .pipe(zip(`Groupify-Auto-Tab-Organizer-${pkg.version}.zip`))
   .pipe(gulp.dest('package'))
